01117ba3deb4e5f9faae11c3edc76d5efcabd5c5
Gijs Machielsen <g.machielsen@gmail.com>
2018-11-28 20:34:23 +0000

    end november commit

--- js/newindex.js
+++
@@ -1,9 +0,0 @@
-// $(function(){  // $(document).ready shorthand
-//   $('DIV').fadeIn('slow');
-// });
-
-$(document).ready(function(){
-    $("DIV").scroll(function(){
-        $("DIV").fadeIN()
-    });
-});
